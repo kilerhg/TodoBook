d5f0fa20c09b8c5f94d08a19ea84299d7ab13e3d
Lucas Nunes <64300982+kilerhg@users.noreply.github.com>
2021-09-12 18:52:27 -0300

    Finishing Login Stufs, adding Crud Books
    Login with OAuth google,
    CRUD of books.

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 from authlib.integrations.flask_client import OAuth
-import requests
+from datetime import timedelta
 import os
+import secrets
 
 import funcoes
 import dao
 
+import logging
+
+from auth_decorator import login_required
+
+# dotenv setup
+from dotenv import load_dotenv
+load_dotenv()
+
+secret = secrets.token_urlsafe(32)
 
 app = Flask(__name__)
-app.secrect_key = '123'
+# app.secrect_key = secret
+# app.secrect_key = os.getenv("APP_SECRET_KEY")
+app.config['SECRET_KEY'] = os.getenv("APP_SECRET_KEY")
+
+
+app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
 
 oauth = OAuth(app)
 google = oauth.register(
@@ -20,6 +36,7 @@ google = oauth.register(
     authorize_url='https://accounts.google.com/o/oauth2/auth',
     authorize_params=None,
     api_base_url='https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',
     client_kwargs={'scope': 'openid email profile'},
 )
 
@@ -30,19 +47,28 @@ def index():
 
 @app.route('/login')
 def login():
-    google = oauth.create_client('google')
+    google = oauth.create_client('google')  # create the google oauth client
     redirect_uri = url_for('authorize', _external=True)
     return google.authorize_redirect(redirect_uri)
 
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect('/')
+
 @app.route('/authorize')
 def authorize():
-    google = oauth.create_client('google')
-    token = google.authorize_access_token()
-    resp = google.get('userinfo')
-    resp.raise_for_status()
+    google = oauth.create_client('google')  # create the google oauth client
+    token = google.authorize_access_token()  # Access token from google (needed to get user info)
+    resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
     user_info = resp.json()
-    session['email'] = user_info['email']
-    # do something with the token and profile
+    user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
+    # Here you use the profile/user data that you got and query your database find/register the user
+    # and set ur own data in the session not the profile from google
+    session['profile'] = user_info
+    session['user'] = user
+    # session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
     return redirect('/')
 
 @app.route("/busca", methods=["GET", "POST"])
@@ -51,19 +77,44 @@ def search():
     if request.method == "POST":
         livro = request.form["book_name"]
         lista_livros = funcoes.search_book(busca=livro)
+        print(lista_livros)
     return render_template('search.html', books=lista_livros)
 
 
 @app.route("/biblioteca")
+@login_required
 def library():
-    return render_template('library.html')
+    db, cursor = dao.connect_db()
+    id_user = dict(session)['profile']['id']
+    list_books_id = dao.get_id_books_by_user_id(cursor=cursor, id_user=id_user)
+    list_books = funcoes.get_books_by_id(list_books_id=list_books_id)
+    return render_template(
+        'library.html',
+        user_session=dict(session)['profile'],
+        list_books=list_books,
+        )
 
+@app.route("/biblioteca/add/<book_id>")
+@login_required
+def add_book_library(book_id):
+    db, cursor = dao.connect_db()
 
-@app.route("/biblioteca/<int:unique>")
-def add_book_library(isbn):
-    print(isbn)
-    return render_template('library.html')
+    id_user = dict(session)['profile']['id']
 
+    book_exist = dao.get_id_register(cursor=cursor, id_user=id_user, google_book_id=book_id)
+    
+    if not book_exist:
+        dao.insert_book_into_library_by_id(db=db, cursor=cursor, id_user=id_user, google_book_id=book_id)
+    return redirect('/biblioteca')
+
+@app.route("/biblioteca/remove/<book_id>")
+@login_required
+def remove_book_library(book_id):
+    db, cursor = dao.connect_db()
+
+    id_user = dict(session)['profile']['id']
+    dao.remove_book_from_library_by_id(db=db, cursor=cursor, id_user=id_user, google_book_id=book_id)
+    return redirect('/biblioteca')
 
 
 # @app.route("/cadastrar", methods=["GET", "POST"])
--- dao.py
+++ dao.py
@@ -1,6 +1,5 @@
 import sqlite3
 import os
-from werkzeug.security import generate_password_hash, check_password_hash
 
 def connect_db(name='database'):
 
@@ -10,14 +9,6 @@ def connect_db(name='database'):
     else:
         db = sqlite3.connect(f"{name}.db")
         cursor = db.cursor()
-
-        sql_query_generate_users = '''
-            CREATE TABLE users (
-                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                username TEXT(50) NOT NULL,
-                email TEXT(50) NOT NULL UNIQUE,
-                password TEXT(200) NOT NULL
-            );'''
 
         sql_query_generate_status_book = '''
             CREATE TABLE status_book (
@@ -32,7 +23,6 @@ def connect_db(name='database'):
                 book_unique_key TEXT(40),
                 id_status_book INTEGER,
                 percent_book FLOAT(3),
-                FOREIGN KEY(id_user) REFERENCES users(id)
                 FOREIGN KEY(id_status_book) REFERENCES status_book(id_status)
             );'''
 
@@ -42,86 +32,73 @@ def connect_db(name='database'):
             ('1', 'Reading'),
             ('2', 'Readed');'''
 
-        cursor.execute(sql_query_generate_users)
         cursor.execute(sql_query_generate_status_book)
         cursor.execute(sql_query_generate_library)
 
         cursor.execute(sql_query_insert_status_book)
         db.commit()
-
-        
     
     return db, cursor
 
-def insert_user(db, cursor, username : str, email : str, pwd : str):
-    hash_password = generate_password_hash(pwd)
-    query_insert = f"""
-    insert into users(username, email, password) 
-    values ('{username}', '{email}', '{hash_password}');"""
-    try:
-        cursor.execute(query_insert)
-    except sqlite3.IntegrityError:
-        return 'E-mail ja cadastrado'
 
+def insert_book_into_library_by_id(db, cursor, id_user, google_book_id):
+    sql_query_insert_book_into_library = f'''
+            insert into library(id_user, book_unique_key, id_status_book, percent_book) values 
+            ('{id_user}', '{google_book_id}', '0', '0');'''
+    cursor.execute(sql_query_insert_book_into_library)
     db.commit()
 
-def login_user(cursor, email, pwd):
-    query_login = f'''
-    select password from users where email = '{email}';
+
+def remove_book_from_library_by_id(db, cursor, id_user, google_book_id):
+    id_register = get_id_register(cursor=cursor, id_user=id_user, google_book_id=google_book_id)
+    sql_query_remove_book_into_library = f'''
+    delete from library
+    where id_register='{id_register}'
     '''
-    
-    cursor.execute(query_login)
+    cursor.execute(sql_query_remove_book_into_library)
+    db.commit()
 
 
-    hash_password = cursor.fetchone()
-
-    if hash_password:
-        if check_password_hash(hash_password[0], pwd):
-            return True
-        else:
-            return 'senha incorreta'
+def get_id_register(cursor, id_user, google_book_id):
+    sql_query_get_id_register = f'''
+            select id_register from library
+            where id_user = '{id_user}'
+            and book_unique_key = '{google_book_id}';'''
+    cursor.execute(sql_query_get_id_register)
+    register_id = cursor.fetchone()
+    if register_id:
+        register_id = register_id[0]
     else:
-        return 'email não existente'
+        register_id = None
+    return register_id
 
-def get_id_by_email(cursor, email : str):
-    query = f'''
-        select (id) from users
-        where email = '{email}';
-    '''
-    cursor.execute(query)
-    value = cursor.fetchone()
 
-    if value:
-        value = value[0]
+def update_status_book_by_id(cursor, db, new_status_book, id_register):
+    sql_query_update_status_book = f'''
+            update library
+            set id_status_book = {new_status_book}
+            where id_register={id_register}'''
+    cursor.execute(sql_query_update_status_book)
+    db.commit()
+
+
+def update_percent_book_by_id(cursor, db, new_percent, id_register):
+    sql_query_update_percent_book = f'''
+            update library
+            set percent_book = {new_percent}
+            where id_register = {id_register};'''
+    cursor.execute(sql_query_update_percent_book)
+    db.commit()
+
+def get_id_books_by_user_id(cursor, id_user):
+    cursor.execute(f'''
+        select book_unique_key from library
+        where id_user = '{id_user}'
+    ;'''
+    )
+    valores = cursor.fetchall()
+    if valores:
+        valores = [book[0] for book in valores]
     else:
-        value = None
-
-    return value
-        
-
-def update_password_by_id(cursor, db, id_user, password : str):
-    query = f'''
-    UPDATE users
-    set password = '{password}'
-    where id = '{id_user}'
-    '''
-    cursor.execute(query)
-    db.commit()
-
-def update_email_by_id(cursor, db, id_user, email : str):
-    query = f'''
-    UPDATE users
-    set email = '{email}'
-    where id = '{id_user}'
-    '''
-    cursor.execute(query)
-    db.commit()
-
-def update_username_by_id(cursor, db, id_user, username : str):
-    query = f'''
-    UPDATE users
-    set username = '{username}'
-    where id = '{id_user}'
-    '''
-    cursor.execute(query)
-    db.commit()
+        valores = []
+    return valores
